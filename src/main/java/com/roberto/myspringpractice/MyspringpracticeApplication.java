5bbb4c56810c8187c553bfd94a41cc0566cfc971
robertolago <robertolagob@gmail.com>
2019-02-19 10:17:47 -0600

    Initial commit

---
+++ src/main/java/com/roberto/myspringpractice/MyspringpracticeApplication.java
@@ -0,0 +1,13 @@
+package com.roberto.myspringpractice;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MyspringpracticeApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(MyspringpracticeApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/roberto/myspringpractice/MyspringpracticeApplicationTests.java
@@ -0,0 +1,16 @@
+package com.roberto.myspringpractice;
+
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.junit4.SpringRunner;
+
+@RunWith(SpringRunner.class)
+@SpringBootTest
+public class MyspringpracticeApplicationTests {
+
+	@Test
+	public void contextLoads() {
+	}
+
+}
